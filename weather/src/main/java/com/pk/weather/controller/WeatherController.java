9d6c8a34870002d1d0c102908c101330645a3ece
ronbun <82181753+ronbun@users.noreply.github.com>
2022-03-18 03:00:24 +0100

    Add weather service skeleton

---
+++ weather/src/main/java/com/pk/weather/controller/WeatherController.java
@@ -0,0 +1,28 @@
+package com.pk.weather.controller;
+
+import com.pk.weather.models.Weather;
+import com.pk.weather.service.WeatherService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class WeatherController {
+
+  WeatherService weatherService;
+
+  @GetMapping("/oneDay")
+  // TODO add validation
+  public Weather getOneDayWeatherInfo(@RequestParam String lat, @RequestParam String lon, @RequestParam String apiKey) {
+    // TODO use advice insted
+    try {
+      return weatherService.getOneDay(lat, lon, apiKey);
+    } catch (Exception e) {
+      return null;
+    }
+  }
+}
---
+++ weather/src/main/java/com/pk/weather/service/WeatherService.java
@@ -0,0 +1,40 @@
+package com.pk.weather.service;
+
+import com.pk.weather.models.Weather;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class WeatherService {
+  
+  private static final String API_URL_ONE_DAY_WEATHER = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}";
+
+  @Value("${shinano_weather_api_key}")
+  private String apiKey;
+
+  @NonNull private RestTemplate restTemplate;
+
+  public Weather getOneDay(String lat, String lon, String apiKey) throws Exception {
+    String apiUrlOneDayWeather = API_URL_ONE_DAY_WEATHER;
+    apiUrlOneDayWeather = apiUrlOneDayWeather.replace("{lat}", lat);
+    apiUrlOneDayWeather = apiUrlOneDayWeather.replace("{lon}", lon);
+    apiUrlOneDayWeather = apiUrlOneDayWeather.replace("{API_key}", apiKey);
+    ResponseEntity<Weather> weather = restTemplate.getForEntity(apiUrlOneDayWeather, Weather.class);
+    log.debug(weather.toString());
+    if (weather.getBody() == null) {
+      throw new Exception("Weather body is null");
+    }
+    log.debug(weather.getBody().toString());
+    return weather.getBody();
+  }
+}
